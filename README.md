081e7535fdacf21bb3607be8a98ebd89d70a685c
Jon Ho <housuilun@yahoo.com>
2014-09-16 22:37:19 -0700

    separated functionality and files

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 Play around with AngularJS and Firebase.
 
 Following the tutorial for Chat App based off of AngularFire.
+
+## Features I will be adding/contemplating
+* Chat rooms
+* Chat room tabs
---
+++ scripts/controllers.js
@@ -0,0 +1,21 @@
+angular.module('myApp.controllers', ["firebase"])
+	.controller('MyController', ['$scope', '$firebase', function($scope, $firebase) {
+		var ref = new Firebase("https://playwithfire.firebaseIO.com/");
+		$scope.messages = $firebase(ref).$asArray();
+
+		//ADD MESSAGE METHOD
+		$scope.addMessage = function(e) {
+
+			//LISTEN FOR RETURN KEY
+			if (e.keyCode === 13 && $scope.msg) {
+				//ALLOW CUSTOM OR ANONYMOUS USER NAMES
+				var name = $scope.name || 'anonymous';
+
+				// add msg
+				$scope.messages.$add({from: name, body: $scope.msg});
+
+				//RESET MESSAGE
+				$scope.msg = "";
+			}
+		}
+	}]);
