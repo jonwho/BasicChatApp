bcc1af149559d453adb5b42dd3ad72ece13d16e7
Jonathan Ho <housuilun@yahoo.com>
2014-09-24 19:54:41 +0000

    work on moving controller

--- scripts/chat.js
+++ scripts/chat.js
@@ -1,1 +1,19 @@
 // handle single chat instance
+
+'use strict';
+
+angular.module('myApp')
+    .controller('ChatController', function($scope, $firebase) {
+        var ref = new Firebase('https://playwithfire.firebaseIO.com/');
+        $scope.messages = $firebase(ref).$asArray();
+        
+        $scope.addMessage = function (e) {
+            if (e.keyCode === 13 && $scope.msg) {
+                var name = $scope.name || 'anonymous';
+                
+                $scope.messages.$add({from: name, body: $scope.msg});
+                
+                $scope.msg = '';
+            }
+        }
+    });
